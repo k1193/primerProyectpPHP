4a87418285173bbad8027fb4dea25beb0f324c20
501 <501@academicos.cesde.edu.co>
2020-02-14 21:00:49 -0500

    esta es la version funcional de mi codigo

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+ // programar con php
+ 
+ /* este es un comentario */
+/*las variables se nombran inicianndo con una letra*/
+/*echo variable paraa imprimir y se coloca   despues */
+
+$nombre= "ksandra";
+echo($nombre);
+
+
+$edad=17;
+echo($edad);
+echo("<br>");
+$estatura=1.59;
+echo($nombre);
+
+
+
+//$edad=125;
+echo($edad);
+echo("<br>");
+
+echo("hola mundo");
+echo("<br>");
+
+
+echo("Mi nombre es:".$nombre. " y la edad es:" .$edad );
+
+
+
+
+
+
+
+
+
+?>
